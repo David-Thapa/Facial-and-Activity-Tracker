8253c3cc8b9b6482ef3ba8c3e5ae507ee5db23f3
Tanaya-Shrestha <tanayastha2000@gmail.com>
2024-01-31 05:39:41 +0545

    folder structure 0.5, added uploadphotopage

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import Logo1 from '../assests/images/Logo1.png'
+import "../assests/style/AboutPage.css"
+import { Link } from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
+import { useState } from 'react';
+import TwoBirds from '../assests/images/TwoBirds.jpg'
 
 const AboutPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>This is About page</h1>
+    <div className="avian-vision">
+      <div className="header">
+        <nav className="navigation">
+          <Link to='/'>
+            <img src={Logo1} alt="Logo is AvianVision" className="Logo-avian" />
+          </Link>                
+          <ul className='middle'>
+            <li><Link to="AboutPage">About</Link></li>
+            <li><Link to="UploadPhoto">Upload Photo</Link></li>
+            <li><Link to="#">Leave a Message</Link></li>
+          </ul>
+          <ul className='last'>
+            <li><button className="log-out-button" onClick={() => navigate("/")}>Log out</button></li>
+          </ul>
+        </nav>
+      </div>
+      <div className='hero'>
+        <div className='hero-left'>
+          <p>Welcome to AvianVision - your go-to destination for seamless bird identification and exploration! At AvianVision, our passion for connecting people with the natural world drives us. As avid birdwatchers and technology enthusiasts, we envisioned a seamless bridge between the beauty of birdwatching and the power of cutting-edge technology. Established with a commitment to making bird identification accessible to all, AvianVision combines user-friendly design with advanced CNN models, ensuring accurate and instant results.
+          </p>
+          <div className='hero-right'>
+            <img src={TwoBirds} alt='Two birds photo' />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
